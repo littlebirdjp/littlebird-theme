736c0b72f7dd041f8e3b35038de1606be83ebdb1
Yusuke Takahashi <youthkee@gmail.com>
2014-12-23 21:47:15 +0900

    投稿日と投稿者のスタイル変更

--- wp-content/themes/littlebird/inc/template-tags.php
+++ wp-content/themes/littlebird/inc/template-tags.php
@@ -79,13 +79,15 @@ function littlebird_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		_x( 'Posted on %s', 'post date', 'littlebird' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		_x( '%s', 'post date', 'littlebird' ),
+//		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		'<i class="fa fa-calendar"></i> ' . $time_string
 	);
 
 	$byline = sprintf(
-		_x( 'by %s', 'post author', 'littlebird' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		_x( '%s', 'post author', 'littlebird' ),
+//		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		'<i class="fa fa-user"></i> <span class="author vcard">' . esc_html( get_the_author() ) . '</span>'
 	);
 
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
