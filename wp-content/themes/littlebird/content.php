a2b78e6883a98b3526c3a84bfbc8fb70396308b6
Yusuke Takahashi <youthkee@gmail.com>
2014-12-30 10:44:37 +0900

    「続きを読む」リンクのデザイン修正

--- wp-content/themes/littlebird/content.php
+++ wp-content/themes/littlebird/content.php
@@ -19,7 +19,7 @@
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'littlebird' ),
+				__( 'Continue reading %s <i class="meta-nav fa fa-angle-right"></i>', 'littlebird' ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 		?>
